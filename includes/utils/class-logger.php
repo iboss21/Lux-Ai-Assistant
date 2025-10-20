14eb84a5340ad2972bab95226334cf33a9923625
Master Mind <48193931+iboss21@users.noreply.github.com>
2025-10-19 21:38:38 -0400

    Add Logger class for logging messages

---
+++ includes/utils/class-logger.php
@@ -0,0 +1,10 @@
+<?php
+namespace LuxAI\Utils;
+
+
+class Logger {
+public static function log($level,$context,$message){ global $wpdb; $table=$wpdb->prefix . \LUXAI_LOG_TABLE; $wpdb->insert($table,[ 'created_at'=>current_time('mysql'),'level'=>sanitize_text_field($level),'actor'=>wp_get_current_user()->user_login?:'system','context'=>sanitize_text_field($context),'message'=>$message ]); }
+public static function info($ctx,$msg){ self::log('info',$ctx,$msg); }
+public static function warn($ctx,$msg){ self::log('warn',$ctx,$msg); }
+public static function error($ctx,$msg){ self::log('error',$ctx,$msg); }
+}
